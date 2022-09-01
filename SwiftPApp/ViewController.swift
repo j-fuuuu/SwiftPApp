57a0ea12641f79651a45092871a8db9b1cfcb2a8
kumi fujii <kuum9393@gmail.com>
2022-09-01 10:09:03 +0900

    0901

--- SwiftPApp/ViewController.swift
+++ SwiftPApp/ViewController.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
+import PKHUD
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    //画面が表示された直後に呼び出される
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //PKHUDを表示
+        HUD.flash(.success, delay:2.0)
     }
 
 
